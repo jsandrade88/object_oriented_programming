98b3d1c072a06d113682df7f41a7709aca26fb0c
jsandrade88 <julioandrade@mail.weber.edu>
2022-11-29 19:00:57 -0700

    cars

--- Vehicle.py
+++ Vehicle.py
@@ -1,22 +1,19 @@
 class Vehicle:
-    pass
+    
+    #class level attribute 
+    type = "ground"
+    propulsion = "battery"
 
-bug_object = Vehicle() #object of vehicle class -- instance of vehicle class
-turtle_object = Vehicle()
-rover_object = Vehicle()
 
-bug_object.color = "yellow"
-bug_object.num_wheels = 4
-bug_object.speed = 1
+    def __init__(self, name, color, num_wheels, speed):
+        self.name = name
+        self.color = color
+        self.num_wheels = num_wheels
+        self.speed = speed
+    
+    def print_details(self):
+        print(self.name, "is", self.color, "and is able to drive", self.speed, "mph")
 
-turtle_object.color = "green"
-turtle_object.num_wheels = 2
-turtle_object.speed = 5
-
-rover_object.color = "purple"
-rover_object.num_wheels = 4
-rover_object.speed = 25
-
-print("This vehicle is", bug_object.color, "and is able to drive", bug_object.speed, "mph")
-print("This vehicle is", turtle_object.color, "and is able to drive", turtle_object.speed, "mph")
-print("This vehicle is", rover_object.color, "and is able to drive", rover_object.speed, "mph")
+    def paint_vehicle(self, newcolor):
+        self.color = newcolor
+       
